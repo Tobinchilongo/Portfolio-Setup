d511cafe88b4e8b1caee4f766d7ce46a74cb6223
jihaneh <jijih@hotmail.fr>
2021-12-13 15:23:03 +0100

    Add closeMenu JS function that close the menu in mobile version

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,14 @@ let menu_button = document.querySelector(".mobile-nav");
 menu_button.addEventListener("click", openMenu);
 
 function openMenu() {
-    let mobile_menu = document.querySelector(".navbar-mobile")
-    mobile_menu.classList.remove("navbar_hidden");
+  let mobile_menu = document.querySelector(".navbar-mobile");
+  mobile_menu.classList.remove("navbar_hidden");
+}
+
+let menu_close_button = document.querySelector(".navbar-mobile-close");
+menu_close_button.addEventListener("click", closeMenu);
+
+function closeMenu() {
+  let mobile_navbar = document.querySelector(".navbar-mobile");
+  mobile_navbar.classList.add("navbar_hidden");
 }
