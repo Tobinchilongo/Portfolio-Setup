b38bfcdb20dac567643a1a5e10b716fef66a8219
tobin <tobinchilongojr@gmail.com>
2021-12-13 17:41:44 +0200

    Fix linters

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,13 @@
-const menuButton = document.querySelector('.mobile-nav');
-menuButton.addEventListener('click', openMenu);
-
 function openMenu() {
   const mobileMenu = document.querySelector('.navbar-mobile');
   mobileMenu.classList.remove('navbar_hidden');
 }
+const menuButton = document.querySelector('.mobile-nav');
+menuButton.addEventListener('click', openMenu);
 
 function closeMenu() {
-    const mobileNavbar = document.querySelector('.navbar-mobile');
-    mobileNavbar.classList.add('navbar_hidden');
-  }
+  const mobileNavbar = document.querySelector('.navbar-mobile');
+  mobileNavbar.classList.add('navbar_hidden');
+}
 const menuCloseButton = document.querySelector('.navbar-mobile-close');
 menuCloseButton.addEventListener('click', closeMenu);
-
