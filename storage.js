d483dab9e8609c1b952e724f385abe63626e7750
jihaneh <jijih@hotmail.fr>
2021-12-16 14:08:25 +0100

    Fix object properties name

--- storage.js
+++ storage.js
@@ -1,24 +1,25 @@
 let formData = {
-    FirstName: '',
-    email: '',
-    message: '',
-    LastName: '',
-  };
-  
-  const form = document.querySelector('form');
-  
-  if (localStorage.getItem('formData') != null) {
-    formData = JSON.parse(localStorage.getItem('formData'));
-    form.firstname.value = formData.FirstName;
-    form.email.value = formData.email;
-    form.message.value = formData.message;
-    form.lastName.value = formData.LastName;
-  }
-  
-  form.addEventListener('submit', () => {
-    formData.FirstName = form.firstName.value;
-    formData.email = form.email.value;
-    formData.message = form.message.value;
-    formData.LastName = form.lastName.value;
-    localStorage.setItem('formData', JSON.stringify(formData));
-  });
+  firstName: "",
+  email: "",
+  message: "",
+  lastName: "",
+};
+
+const form = document.querySelector("form");
+
+if (localStorage.getItem("formData") != null) {
+  formData = JSON.parse(localStorage.getItem("formData"));
+  form.firstname.value = formData.firstName;
+  form.email.value = formData.email;
+  form.message.value = formData.message;
+  form.lastname.value = formData.lastName;
+  console.log(form);
+}
+
+form.addEventListener("submit", () => {
+  formData.firstName = form.firstname.value;
+  formData.email = form.email.value;
+  formData.message = form.message.value;
+  formData.lastName = form.lastname.value;
+  localStorage.setItem("formData", JSON.stringify(formData));
+});
