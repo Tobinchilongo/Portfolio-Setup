88a7ea91206ffaf7e2a418eef4c45ea707d16bbf
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-01-22 11:15:33 +0200

    Add two projects

--- storage.js
+++ storage.js
@@ -1,25 +1,24 @@
 let formData = {
-  firstName: "",
-  email: "",
-  message: "",
-  lastName: "",
+  firstName: '',
+  email: '',
+  message: '',
+  lastName: '',
 };
 
-const form = document.querySelector("form");
+const form = document.querySelector('form');
 
-if (localStorage.getItem("formData") != null) {
-  formData = JSON.parse(localStorage.getItem("formData"));
+if (localStorage.getItem('formData') != null) {
+  formData = JSON.parse(localStorage.getItem('formData'));
   form.firstname.value = formData.firstName;
   form.email.value = formData.email;
   form.message.value = formData.message;
   form.lastname.value = formData.lastName;
-  console.log(form);
 }
 
-form.addEventListener("submit", () => {
+form.addEventListener('change', () => {
   formData.firstName = form.firstname.value;
   formData.email = form.email.value;
   formData.message = form.message.value;
   formData.lastName = form.lastname.value;
-  localStorage.setItem("formData", JSON.stringify(formData));
+  localStorage.setItem('formData', JSON.stringify(formData));
 });
